875139138edd33d69ed36fae4cc792eea3fa3a20
MoMo R <moran.risk78@gmail.com>
2022-10-06 16:14:37 -0700

    Simplify HTML & JS using only data-hour attribute

--- script.js
+++ script.js
@@ -1,76 +1,116 @@
 var today = moment();
+// get the hour number in 0-24.
 var thisHour = moment().format('k');
 
-console.log(thisHour);
+// local storage variable will have this prefix following after the hour number eg. sch-text-9
+var localStoragePrefix = 'sch-text-';
+
+
+// to refresh every top of the hour
+var thisMinute = moment().format('m');
+var minutesLeftToRefresh = 60 - thisMinute;
+
+
+console.log(thisHour+':'+thisMinute);
+console.log('minutes left: '+minutesLeftToRefresh);
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+var clickedHour;
 
-// save butns
+// When 'save' button is clicked
 $('.saveBtn').on('click', function(){
-    // console.log($(this).siblings( "textarea" ).val());
-    var txtInserted = $(this).siblings( "textarea" ).val();
 
-    // console.log($(this).siblings( "textarea" ).attr('id'));
-    var txtareaId = $(this).siblings( "textarea" ).attr('id');
-    localStorage.setItem(txtareaId, txtInserted);
-    
-    // $('.added').show();
-    // setTimeout(function() {
-    //     $('.added').hide();
-    //   }, 1000 );
-    var timeBlockNum = $(this).parent().attr('data-hour');
-    var addMsgId = '#added-'+timeBlockNum; 
-    console.log(addMsgId);
-    console.log($(addMsgId));
-    $(addMsgId).show();
+    var timeBlockHour = $(this).parent().attr('data-hour');
+    var storageKey =  localStoragePrefix+timeBlockHour ;
+
+    console.log("key:"+storageKey);
+    // get the inserted text
+    var txtInserted = $(this).siblings('textarea').val();
+
+    // get the Local storage data to compare new text
+    var oldText = localStorage.getItem(storageKey);
+
+    // this is the <p> to show the updated result message
+    var msgEl = $(this).siblings('.added');
+
+    if ( txtInserted  == oldText ) {
+        msgEl.text('No Change.');   // don't need to update the storage
+    } else {
+        msgEl.text('Schedule updated on the local storage✍');
+        localStorage.setItem(storageKey, txtInserted);       
+    }
+
+
+    // var txtareaId = $(this).siblings( "textarea" ).attr('id');
+    // var txtInserted = $(this).siblings( "textarea" ).val();
+
+    // // get the Local storage data to compare new text
+    // var oldText = localStorage.getItem(txtareaId);
+
+    // // get .added message element by ID
+    // var timeBlockNum = $(this).parent().attr('data-hour');
+    // var addMsgId = '#added-'+timeBlockNum; 
+
+    // // assign the message accordingly and store on localstorage when there is change    
+    // if ( txtInserted  == oldText ) {
+    //     $(addMsgId).text('No Change.');
+    // } else {
+    //     $(addMsgId).text('Schedule updated on the local storage✍');
+    //     localStorage.setItem(txtareaId, txtInserted);
+        
+    // }
+   
+    // show the updated message and hide in 1 sec
+    msgEl.show();
     setTimeout(function() {
-        $(addMsgId).hide();
-     }, 1000 );
-
-})
-
-function callback() {
-    setTimeout(function() {
-      $( ".added" ).removeAttr( "style" ).hide().fadeIn();
+        msgEl.hide();
     }, 1000 );
-  };
 
+})
+
+
+// style changes for buttons mouseover / out - 
+// highlight the textarea when the button is mouseovred
 $('.saveBtn').on('mouseover', function(){
-    $(this).children().eq(0).removeClass().addClass('far fa-save');
-
+    $(this).children('i').removeClass().addClass('far fa-save');
+    $(this).siblings('textarea').css('font-weight','bold');
 })
-
 $('.saveBtn').on('mouseout', function(){
-    $(this).children().eq(0).removeClass().addClass('fas fa-save');
-
+    $(this).children('i').removeClass().addClass('fas fa-save');
+    $(this).siblings('textarea').css('font-weight','normal');
 })
 
 
 
+// each time-block div, 
+// fill out the textarea with local storage value 
+// and style based on the data-hour value
 var timeblockArr = $('.time-block');
 $.each( timeblockArr, function(){
     
-    console.log($(this));
-    var textareaEl = $(this).children().eq(1);
+    var textareaEl = $(this).children('textarea');
 
-    // had to use parseInt to compare the time correctly
+    // ParseInt to compare with current time
     var timeBlockHour = parseInt($(this).attr('data-hour'));
-    console.log('timeblock: '+timeBlockHour);
-    console.log('thishour: '+thisHour);
 
     if (timeBlockHour == thisHour ) {
-        console.log('CURRENT TIME');
+        // console.log('CURRENT TIME');
         textareaEl.removeClass().addClass('col-10 present');
     } else if ( timeBlockHour < thisHour ) {
-        console.log('PAST TIME');
+        // console.log('PAST TIME');
         textareaEl.removeClass().addClass('col-10 past');
     } else if ( timeBlockHour > thisHour ) {
-        console.log('FUTURE TIME');
+        // console.log('FUTURE TIME');
         textareaEl.removeClass().addClass('col-10 future');
     }
     
-    textareaEl.val(localStorage.getItem(textareaEl.attr('id')));
-
-
+    var storageKey =  localStoragePrefix+timeBlockHour ;
+    textareaEl.val(localStorage.getItem(storageKey));
 
 })
+
+// refresh when the hour changes
+setInterval( function(){
+    window.location.reload(true);
+}, minutesLeftToRefresh*60000);
+
